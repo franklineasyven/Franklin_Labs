d85ed63c2f233c22d07cbdcb6e1d80144adc7616
franklineasyven <franklin.easyven@gmail.com>
2025-04-21 13:35:48 -0600

    Initial commit

--- script.js
+++ script.js
@@ -7,6 +7,29 @@ const products = [
 
 const productList = document.getElementById('productList');
 const searchBox = document.getElementById('searchBox');
+const modal = document.getElementById('productModal');
+const modalImage = document.getElementById('modalImage');
+const modalTitle = document.getElementById('modalTitle');
+const modalDescription = document.getElementById('modalDescription');
+const closeBtn = document.querySelector('.close-btn');
+
+function showProductModal(product) {
+  modalImage.src = product.image;
+  modalTitle.textContent = product.name;
+  modalDescription.textContent = product.description;
+  modal.style.display = 'flex';
+}
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', e => {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+});
+
 
 function displayProducts(filtered = products) {
   productList.innerHTML = '';
